fc30153bf27b957e805647379cb1bf18db1c37e8
Laxmi S Kallimath <38729902+laxmiskallimath@users.noreply.github.com>
2024-03-09 19:29:08 +0530

    Update User Engagement Analysis.sql

--- User Engagement Analysis.sql
+++ User Engagement Analysis.sql
@@ -1,6 +1,11 @@
--- ********** User Engagement Analysis: Problem Statement ************************
+-- ********** User Engagement Analysis ************************
+
+-- Problem Statement: 
 
 -- Write SQL queries to gain insights into user engagement by addressing the following problems:
+--  1.Retrieve the comprehensive count of likes, comments, and shares garnered by a specific post identified by its unique post ID.
+--  2.Calculate the mean number of reactions, encompassing likes, comments, and shares, per distinct user within a designated time period.
+--  3.Identify the three most engaging posts, measured by the aggregate sum of reactions, within the preceding week.
 
 -- ********************************* IN MYSQL *****************************************
 
